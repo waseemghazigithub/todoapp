54bd8fef2cf90b2dbf1b7c7e1045fea21ca5f50c
waseemghazigithub <159131359+waseemghazigithub@users.noreply.github.com>
2024-11-26 11:41:46 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-center text-[35px] underline"> Movies Todo App  </h1>
+      <h1 className="text-center text-[35px] underline"> Movies Todo App Test  </h1>
       {/* start input div */}
        <div className="flex justify-between gap-4 mt-4">
         <input type="text" value={inputval}
