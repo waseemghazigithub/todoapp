e72fbccaa7017fe837abf2196a08f61b57c015a9
waseemghazigithub <159131359+waseemghazigithub@users.noreply.github.com>
2024-11-26 13:18:31 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
     <div className="max-w-4xl mx-auto p-4">
       <h1 className="text-center text-[35px] underline"> Movies Todo App </h1>
       {/* start input div */}
-       <div className="flex justify-between gap-4 mt-4">
+       <div className=" flex justify-between gap-4 mt-4">
         <input type="text" value={inputval}
         
         onChange={(e) => setinputval(e.target.value)}
@@ -58,7 +58,7 @@ export default function Home() {
         <input 
         type="number" value={id}
         onChange={(e:any) => setid(e.target.value)}
-        className="p-2 ml-2 w-[20%] border-b text-lg focus:outline-none " 
+        className="bt-c p-2 ml-2 w-[20%] border-b text-lg focus:outline-none " 
         placeholder="Move Id">
         </input>
         <button onClick={(Additme)} className="bg-blue-400 w-[20%] text-white rounded hover:bg-blue-200"> Add movies</button>
@@ -74,7 +74,7 @@ export default function Home() {
       {
         todos.map((item:any,i:any)=>{
           return(
-            <div className="shadow p-4" key={i}>
+            <div className="bgshadow p-4" key={i}>
             <div className="flex justify-between">
               <span className="shadow rounded-full h-8 w-8 text-center my-auto ">{i+1}</span>
               <span onClick={()=>Delitem(item.id)} className="shadow rounded-full h-8 w-8 text-center my-auto cursor-pointer">x</span>
